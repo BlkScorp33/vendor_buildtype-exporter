4d7bbd3253189b5612c690dfa8680b8ea18d3370
Snuzzo <jrracinfan@gmail.com>
2015-08-30 09:55:34 -0400

    Use a single statement for reading input

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,19 +7,15 @@ echo " 4) Weekly"
 echo " 5) Release"
 echo "Choose number selection[6 and above = exit]:"
 read input
-if [ "$input" == "2" ];then
+if [[ "$input" == "2" ]];then
 export BUILDTYPE_EXPERIMENTAL=true
-fi
-if [ "$input" == "3" ];then
+elif [[ "$input" == "3" ]]; then
 export BUILDTYPE_NIGHTLY=true
-fi
-if [ "$input" == "4" ];then
+elif [ "$input" == "4" ];then
 export BUILDTYPE_WEEKLY=true
-fi
-if [ "$input" == "5" ];then
+elif [ "$input" == "5" ];then
 export BUILDTYPE_RELEASE=true
-fi
-if [ "$input" -gt "5" ];then
+elif [ "$input" -gt "5" ];then
 echo "Exiting .... "
 sleep 2
 return
